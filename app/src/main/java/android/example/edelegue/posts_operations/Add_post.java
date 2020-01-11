b14b70ce8fbda942c30ba10f3980cb9b4cfc4ad8
soso <soumiaelhali@gmail.com>
2020-01-11 14:28:34 +0100

    add professor post

---
+++ app/src/main/java/android/example/edelegue/posts_operations/Add_post.java
@@ -0,0 +1,15 @@
+package android.example.edelegue.posts_operations;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.example.edelegue.R;
+import android.os.Bundle;
+
+public class Add_post extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_post);
+    }
+}
