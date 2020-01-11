f4b25c722ce702a62ccf37e7656ac01750f3641d
mehdi ghoulami <ghoulami97@gmail.com>
2020-01-11 12:16:33 +0100

    fix bugs

--- app/src/main/java/android/example/edelegue/MainActivity.java
+++ app/src/main/java/android/example/edelegue/MainActivity.java
@@ -3,6 +3,7 @@ package android.example.edelegue;
 import android.content.Intent;
 import android.example.edelegue.ChatModule.MessageModel;
 import android.os.Bundle;
+import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Toast;
