ac5e3d76cec461d2bad33e95cfe1054cdb38016d
soso <soumiaelhali@gmail.com>
2020-01-13 21:48:01 +0100

    link

---
+++ app/src/main/java/android/example/edelegue/ui/professor_fragments/HomeFragment.java
@@ -0,0 +1,130 @@
+package android.example.edelegue.ui.professor_fragments;
+
+import android.content.Context;
+import android.content.Intent;
+import android.example.edelegue.ChatModule.MessageModel;
+import android.example.edelegue.MainActivity;
+import android.example.edelegue.PostAdapter;
+import android.example.edelegue.User;
+import android.example.edelegue.StudentModule.Post;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import android.example.edelegue.R;
+import android.widget.TextView;
+import android.widget.Toolbar;
+
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class HomeFragment extends Fragment {
+
+    private RecyclerView recyclerView;
+
+    private PostAdapter postAdapter;
+    private List<Post> mPosts;
+
+    CircleImageView profile_image;
+    TextView username;
+
+    FirebaseUser firebaseUser;
+    DocumentReference reference ;
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        recyclerView = view.findViewById(R.id.studientPost_recycler_view);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(view.getContext()));
+
+
+        mPosts = new ArrayList<>();
+
+        readPosts();
+
+        return view;
+
+    }
+
+
+    private void readPosts() {
+        final FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        CollectionReference reference = FirebaseFirestore.getInstance().collection("Posts");
+
+        reference.orderBy("currentTime", Query.Direction.DESCENDING).addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(@NonNull QuerySnapshot value,
+                                @NonNull FirebaseFirestoreException e) {
+
+                if (FirebaseAuth.getInstance().getCurrentUser() != null){
+                    mPosts.clear();
+                    Log.d("dddd" , String.valueOf(value.size()));
+                    for (QueryDocumentSnapshot document : value) {
+
+                        SimpleDateFormat formater = new SimpleDateFormat("'le' dd/MM/yyyy 'à' HH:mm");
+                        String date = formater.format(document.getTimestamp("currentTime").toDate());
+
+                        Post post = new Post(document.getId() , document.getString("user_id") , document.getString("content"), date , document.getString("img_url") , document.getString("title"));
+
+                        assert post != null;
+                        assert firebaseUser != null;
+
+                        mPosts.add(post);
+
+                    }
+                }
+
+
+                postAdapter = new PostAdapter(mPosts);
+                recyclerView.setAdapter(postAdapter);
+            }
+        });
+
+        DocumentReference doc = FirebaseFirestore.getInstance().collection("Posts").document("6s0C7YlRtAki3WsScJgN");
+        doc.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                Log.d("dddd" , String.valueOf(task.getResult().getString("user_id")));
+            }
+        });
+    }
+
+    public interface OnFragmentInteractionListener {
+    }
+}
+
+
+
+
+
